058e3331b10b1b7f77a4fc30cd9948cecc0d164a
Jason Clark Altmann <jason-clark.altmann@outlook.de>
2024-12-21 21:30:32 +0100

    add initial setup

---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Welcome to Your Vue.js + TypeScript App</h1>
+</template>
---
+++ src/views/WeatherView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import WeatherCard from '../components/WeatherCard.vue'
+</script>
+
+<template>
+  <WeatherCard />
+</template>
